9fbb82b760e30eef00c24cfbe59b140c7e892394
Jon Schang <jon_schang@yahoo.com>
2015-10-18 19:55:45 +0000

    flushing out handwriting...gonna need a backend for the networks, tho...takes too long to create a training set and train each time

--- handwriting.js
+++ handwriting.js
@@ -1,24 +1,20 @@
-var network = neuraljs.newNetwork();
-
+var network = null;
 $(document).ready(function() {
-    var o = network;
-    // create the input neurons
-    var endIdx = $('#canvas')[0].width * $('#canvas')[0].height;
-    for(var idx = 0; idx < endIdx; idx++) {
-        var newN = o.newNeuron();
-        o.inputs.push(newN);
-        o.neurons[newN.id] = newN;
+    network = neuraljs.constructors.fullyConnected([28*28,28*28,26]);
+    network.log = function() {};
+    // assign each output to a letter
+    var outputs = network.getOutputs();
+    var letters = {};
+    var j=0;
+    var a = ("a".charCodeAt(0));
+    for(var id in outputs) {
+        letters[ String.fromCharCode( a + j ) ] = id;
+        j++;
     }
-    // fully connect all input neurons to the output neurons
-    var outN = o.newNeuron();
-    o.neurons[outN.id] = outN;
-    for(var i=0; i < o.inputs.length; i++) {
-        var inN = o.inputs[i];
-        var newS = o.newSynapse();
-        inN.addOutput(newS);
-        outN.addInput(newS);
-        o.synapses[newS.id] = newS;
-    }
+    network.hi = 1;
+    network.lo = -1;
+    network.outputIdsByLetter = letters;
+    console.log(network.outputIdsByLetter);
 });
 
 function canvasCoords(evt) {
@@ -77,12 +73,12 @@ $('#canvas').mousemove(function(evt) {
     }
     this.lastCoords = coords;
 });
-$('#add_sample').mouseup(function(evt) {
+$('#add').mouseup(function(evt) {
         
     var primaryCanvas = $('#canvas')[0];
     
     // duplicate the properties of our primary canvas
-    var newSample = $('<div style="clear:both;"><canvas style="border:1px solid blue;"></canvas><div class="run_data"></div></div>')[0];
+    var newSample = $('<div style="clear:both;"><canvas style="border:1px solid blue;"></canvas></div>')[0];
     var newSampleCanvas = $('canvas',newSample)[0];
     newSampleCanvas.width = primaryCanvas.width;
     newSampleCanvas.height = primaryCanvas.height;
@@ -96,8 +92,8 @@ $('#add_sample').mouseup(function(evt) {
         if( imgData.data[i+0]==imgData.data[i+1]
             && imgData.data[i+1]==imgData.data[i+2]
             && imgData.data[i+2]==255 ) {
-            actualData[i/4] = 0;
-        } else actualData[i/4] = 1;
+            actualData[i/4] = network.lo;
+        } else actualData[i/4] = network.hi;
     }
     for(var i=0; i<primaryCanvas.height; i++) {
         var s = "";
@@ -113,6 +109,14 @@ $('#add_sample').mouseup(function(evt) {
     newSampleCtx.createImageData(primaryCanvas.width,primaryCanvas.height);
     newSampleCtx.putImageData(imgData,0,0);
     
+    var outputSelect = $('<select></select>')[0];
+    for(var letter in network.outputIdsByLetter) {
+        var newOpt = $('<option value="'+letter+'">'+letter+'</option>')[0];
+        $(outputSelect).append(newOpt);
+    }
+    $(newSample).append(outputSelect);
+    newSampleCanvas.select = outputSelect;
+    
     // tack the sample on to the end
     $('#training_set').append(newSample);
     
@@ -122,4 +126,27 @@ $('#add_sample').mouseup(function(evt) {
         ctx.fillStyle = 'white';
         ctx.fillRect(0,0,this.width,this.height);
     });
+});
+$('#train').click(function(evt) {
+    var samples = [];
+    $('canvas',$('#training_set')).each(function() {
+        var sample = {}
+        var i=0;
+        for(var id in network.getInputs()) {
+            sample[id] = this.actualData[i];
+            i++;
+        }
+        for(var id in network.getOutputs()) {
+            sample[id] = network.lo;
+        }
+        sample[network.outputIdsByLetter[$(this.select).val()]] = network.hi;
+        console.log('letter = '+$(this.select).val());
+        console.log(sample);
+        samples.push(sample);
+    });
+    var sampler = $N.samplers.shuffler();
+    sampler.samples = samples;
+    network.train(sampler);
+});
+$('#evaluate').click(function(evt) {
 });
